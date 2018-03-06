17ca2a399994f9cef4d1ad27d7deec15af79b45d
Ian King <36984875+classimago@users.noreply.github.com>
2018-03-06 15:18:21 +0000

    Update spectral-workbench.py
    Added processing to make sure the object being passed to spectral workbench is JSON

--- spectral-workbench.py
+++ spectral-workbench.py
@@ -4,7 +4,7 @@ from PIL import Image
 import requests
 import base64
 import urllib3
-
+import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 camera = picamera.PiCamera(framerate = 10)
@@ -18,45 +18,48 @@ camera.zoom = (0.25, 0.25, 0.5, 0.5)
 
 #take the initial spectral reading
 def take_reading(sample_name):
-        camera.start_preview()
-        time.sleep(10)
+  camera.start_preview()
+  time.sleep(10)
   
-#change file path depending on preference
-        camera.capture('/home/pi/Pictures/%s.png' % sample_name)
-        return('/home/pi/Pictures/%s.png' % sample_name)
-        camera.stop_preview()
+  #change file path depending on preference
+  camera.capture('/home/pi/Pictures/%s.png' % sample_name)
+  return('/home/pi/Pictures/%s.png' % sample_name)
+  camera.stop_preview()
 
 #process and encode image for upload
 def process_image(image_path):
-        img = Image.open(image_path)
-        img_width = img.size[0]
-        half_height = img.size[1]/2
-        img2 = img.crop((0, half_height, img_width, half_height + 1))
-        img2.save('/home/pi/Pictures/%s_processed.png' % sample_name)
-        #open processed image in binary
-        with open('/home/pi/Pictures/%s_processed.png' % sample_name, 'rb') as image_file:
-		encoded_image = base64.b64encode(image_file.read())
- 	return encoded_image
+  img = Image.open(image_path)
+  img_width = img.size[0]
+  half_height = img.size[1]/2
+  img2 = img.crop((0, half_height, img_width, half_height + 1))
+  img2.save('/home/pi/Pictures/%s_processed.png' % sample_name)
+	
+  #open processed image in binary
+  with open('/home/pi/Pictures/%s_processed.png' % sample_name, 'rb') as image_file:
+    encoded_image = base64.b64encode(image_file.read())
+  return encoded_image
+
+def jsonDefault(object):
+  return object.decode('utf-8')
 
 def workbench_upload(encoded_image):
-	url = 'https://spectralworkbench.org/spectrums'
+  url = 'https://spectralworkbench.org/spectrums'
 	
-	#replace with your api token
-	api_token = 'your api token here'
+  #replace with your api token
+  api_token = '31353136383034393831'
 	
-	payload = {
-	'spectrum':{
-		'title': sample_name
-		},
-	'dataurl': encoded_image,
-	#'token': api_token
-	}
+  info = {'spectrum':{'title': sample_name},'dataurl': encoded_image}
+  print(type(info))
 	
-	try:
-		r = requests.post(url, data = payload, verify=False)
-		print(r.status_code)
-	except requests.exceptions.RequestException as e:
-		print(e)
+  payload = json.dumps(info, default=jsonDefault)	
+  print(payload)
+  try:
+    r = requests.post(url, data = payload, verify=False)
+    print(r.status_code)
+    print(r.json)
+       
+  except requests.exceptions.RequestException as e:
+    print(e)
 		
 		
 sample_name = input('Enter sample name:')
